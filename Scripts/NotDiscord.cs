1ab4d44106a975819b2c78fedd0ac3e7ad53224b
besturzvr <arthur.peer@htlwy.at>
2025-12-10 16:03:27 +0100

    Button Scripts getestet

---
+++ Scripts/NotDiscord.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NotDiscord : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public float pressDepth = 0.05f;      // Wie tief der Button einsinkt
+    public float animationSpeed = 10f;    // Geschwindigkeit der Rückkehr
+
+    private Vector3 originalPosition;
+    private Vector3 pressedPosition;
+    private bool isPressed = false;
+
+    private void Start()
+    {
+        originalPosition = transform.localPosition;
+        pressedPosition = originalPosition - new Vector3(0, pressDepth, 0);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPressed = true;
+        transform.localPosition = pressedPosition;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPressed = false;
+    }
+
+    private void Update()
+    {
+        if (!isPressed)
+        {
+            // Sanft zurück zur Ausgangsposition gleiten
+            transform.localPosition = Vector3.Lerp(
+                transform.localPosition,
+                originalPosition,
+                Time.deltaTime * animationSpeed
+            );
+        }
+    }
+}
